56f604b4c1049ad737d90e7ca846f7f5aaace13d
pauliiortizz <2202058@ucc.edu.ar>
2025-08-07 15:42:46 -0300

    Agregue una funcion que verifica si un numero es par o no

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
-// Versión inicial
 function saludar() {
     console.log("Hola mundo");
 }
 
 saludar();
+
+function esPar(numero) {
+    return numero % 2 === 0;
+}
+
+console.log(esPar(4));  
+console.log(esPar(7));  
